dfa085ad6c3a69af250dc8d0e74d1029357b9cd6
Josh Endemann <jahend44@gmail.com>
2022-10-09 19:19:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -296,6 +296,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
