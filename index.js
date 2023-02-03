776d6875506c121c4313ae21f89c71efe28aa8d7
Josh Endemann <jahend44@gmail.com>
2023-02-03 15:27:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,22 +21,12 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Import and use CORS but allow all domains to make requests
 const cors = require('cors');
+app.use(express.static('public'));
 app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://mymovieapp44.netlify.app', 'https://jahend412.github.io/myMovie-Angular-client/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+// Import and use Passport
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
