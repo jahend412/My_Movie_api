48da86a3afb8ea8feb48585f07999daef3961345
Josh Endemann <jahend44@gmail.com>
2023-02-03 16:25:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,22 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Import and use CORS but allow all domains to make requests
 const cors = require('cors');
-app.use(express.static('public'));
 app.use(cors());
 
-// Import and use Passport
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://mymovieapp44.netlify.app', 'https://jahend412.github.io'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
